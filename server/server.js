9c2a458ccfde25ee74d570cbae2f8aec461763dd
solannoteste <solannoblack@gmail.com>
2026-06-29 21:47:18 -0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -93,7 +93,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
